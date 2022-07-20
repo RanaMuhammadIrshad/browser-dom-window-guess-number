8cf5f5a882ef191b3fa803d4f427ff8cff896ece
RanaMuhammadIrshad <rmirshad@web.de>
2022-07-20 10:01:58 +0200

    returned the alert without storing in variable

--- index.js
+++ index.js
@@ -6,18 +6,16 @@ const guessNum = function () {
   for (let i = 1; i < 4; i++) {
     myNum = +prompt("guess a number between 1-10");
     if (myNum === num) {
-      const result = alert(`Success, the number was ${num}, attempts: ${i}`);
-      return result;
+      return alert(`Success, the number was ${num}, attempts: ${i}`);
     }
     if (myNum > num || myNum < num) {
       alert("Wrong! Try once more");
     }
   }
   if (myNum !== num) {
-    const c = alert(
+    return alert(
       `Sorry, you failed to guess the number in three attempts. The number was ${num}`
     );
-    return c;
   }
 };
 guessNum();
